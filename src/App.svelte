43a971fe2be3328deeea7bd0aff186383c22b728
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-10-09 17:06:06 -0300

    02-Basic transition
    02-Basic transition

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,30 @@
-<h1>Hello</h1>
+<script>
+    import { fade, blur, slide, scale } from "svelte/transition";
+    let numbers = [];
+
+    function addNumber(){
+        numbers.push(Math.floor(Math.random()*100));
+        numbers = numbers;
+    }
+</script>
+
+<div class="container">
+    <h1>Number:</h1>
+    <div>
+        {#each numbers as number,i(number)}
+            <button class="element" transition:scale>
+                {number}
+            </button>
+        {/each}
+    </div>
+    <hr/>
+    <button on:click={addNumber}>Add a button</button>
+</div>
+
+<style>
+    .element {
+        width: 100%;
+        padding: 10px;
+        margin:5px
+    }
+</style>
